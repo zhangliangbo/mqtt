20fe9b1f6e537ace79d2672df387dbee5e5fd20a
zhangliangbo <2472110501@qq.com>
2018-11-12 12:00:51 +0800

    增加订阅的主题

--- src/main/java/mcivicm/mqtt/Client.java
+++ src/main/java/mcivicm/mqtt/Client.java
@@ -5,19 +5,52 @@ import io.reactivex.disposables.Disposable;
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter host: ");
-        String host = scanner.nextLine();
-        System.out.println("enter id: ");
-        String id = scanner.nextLine();
-        System.out.println("enter publish topic: ");
-        String publish_topic = scanner.nextLine();
-        System.out.println("enter subscribe topic: ");
-        String subscribe_topic = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        String name = "application.properties";
+        InputStream inner = Client.class.getClassLoader().getResourceAsStream(name);
+        if (inner == null) {
+            System.err.println("can not find outer application.properties.");
+        } else {
+            System.out.println("load inner properties.");
+            System.getProperties().load(inner);
+        }
+        String jarDir = System.getProperty("user.dir");
+        System.out.println("jarDir=" + jarDir);
+        File file = new File(jarDir + File.separator + name);
+        System.out.println("outer properties=" + file.getPath());
+        if (file.exists()) {
+            InputStream outer = new FileInputStream(file);
+            System.out.println("load outer properties.");
+            System.getProperties().load(outer);
+            outer.close();
+            String active = System.getProperty("active", "inner");
+            File[] activeFiles = new File(jarDir).listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.contains(active);
+                }
+            });
+            if (activeFiles != null && activeFiles.length > 0) {
+                InputStream activeFile = new FileInputStream(activeFiles[0]);
+                System.out.println("load active properties.");
+                System.getProperties().load(activeFile);
+                activeFile.close();
+            } else {
+                System.err.println("can not find active application.properties.");
+            }
+        } else {
+            System.err.println("can not find outer application.properties.");
+        }
+        String host = System.getProperty("host", "tcp://10.1.1.122:1883");
+        String id = System.getProperty("id", "mqtt");
+        String publish_topic = System.getProperty("publish_topic", "mqtt_publish");
+        String subscribe_topic = System.getProperty("subscribe_topic", "mqtt_subscribe");
+        System.out.println("host=" + host + "|id=" + id + "|publish_topic=" + publish_topic + "|subscribe_topic=" + subscribe_topic);
+
         MQTT.instance().connect(new IOptions.Builder().setHost(host).setId(id).build()).blockingAwait();
         final Disposable[] disposable = new Disposable[1];
         MQTT.instance().subscribeWithTopic(subscribe_topic, 1).subscribe(new Observer<Pair<String, MqttMessage>>() {
@@ -33,7 +66,7 @@ public class Client {
 
             @Override
             public void onError(Throwable e) {
-                System.err.println(e.getMessage());
+                System.out.println(e.getMessage());
             }
 
             @Override
@@ -41,6 +74,7 @@ public class Client {
                 System.out.println("OnComplete");
             }
         });
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("enter quit to quit: ");
             String line = scanner.nextLine();
