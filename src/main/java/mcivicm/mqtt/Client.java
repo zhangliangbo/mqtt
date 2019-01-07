7b907dcb8a15561e653fe3ccbeac5863d8713c3e
zhangliangbo <2472110501@qq.com>
2019-01-07 10:06:47 +0800

    未连接成功时缓存下发的指令

--- src/main/java/mcivicm/mqtt/Client.java
+++ src/main/java/mcivicm/mqtt/Client.java
@@ -9,160 +9,162 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        String name = "application.properties";
-        InputStream inner = Client.class.getClassLoader().getResourceAsStream(name);
-        if (inner == null) {
-            System.err.println("can not find outer application.properties.");
-        } else {
-            System.out.println("load inner properties.");
-            System.getProperties().load(inner);
-        }
-        String jarDir = System.getProperty("user.dir");
-        System.out.println("jarDir=" + jarDir);
-        File file = new File(jarDir + File.separator + name);
-        System.out.println("outer properties=" + file.getPath());
-        if (file.exists()) {
-            InputStream outer = new FileInputStream(file);
-            System.out.println("load outer properties.");
-            System.getProperties().load(outer);
-            outer.close();
-            String active = System.getProperty("active", "inner");
-            File[] activeFiles = new File(jarDir).listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return name.endsWith(".properties") && name.contains("-") && name.substring(name.indexOf("-") + 1, name.indexOf(".")).equals(active);
-                }
-            });
-            if (activeFiles != null && activeFiles.length > 0) {
-                InputStream activeFile = new FileInputStream(activeFiles[0]);
-                System.out.println("load active properties.");
-                System.getProperties().load(activeFile);
-                activeFile.close();
-            } else {
-                System.err.println("can not find active application.properties.");
-            }
-        } else {
-            System.err.println("can not find outer application.properties.");
-        }
-        String host = System.getProperty("host", "tcp://10.1.1.122:1883");
-        String id = System.getProperty("id", "mqtt");
-        String username = System.getProperty("username", "admin");
-        String password = System.getProperty("password", "public");
-        String publish_topic = System.getProperty("publish_topic", "mqtt_publish");
-        String subscribe_topic = System.getProperty("subscribe_topic", "mqtt_subscribe");
-        System.out.println(">>host=" + host);
-        System.out.println(">>username=" + username);
-        System.out.println(">>password=" + password);
-        System.out.println(">>id=" + id);
-        System.out.println(">>publish_topic=" + publish_topic);
-        System.out.println(">>subscribe_topic=" + subscribe_topic);
-        MQTT mqtt = new MQTT();
-        IOptions iOptions = new IOptions.Builder().setHost(host).setId(id).build();
-        iOptions.getOptions().setUserName(username);
-        iOptions.getOptions().setPassword(password.toCharArray());
-        iOptions.getOptions().setAutomaticReconnect(true);
-        mqtt.setMqttCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-                System.err.println(cause.getMessage());
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                System.out.println("messageArrived: " + topic + new String(message.getPayload()));
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-                System.out.println("deliveryComplete: " + (token.getException() == null ? "成功" : token.getException().getMessage()));
-            }
-        });
-        //连接
-        mqtt.connect(iOptions).blockingAwait();
-        AtomicReference<Disposable> disposable = new AtomicReference<>();
-        //订阅
-        mqtt.subscribeWithTopic(subscribe_topic, 2).subscribe(new Observer<Pair<String, MqttMessage>>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposable.set(d);
-            }
-
-            @Override
-            public void onNext(Pair<String, MqttMessage> pair) {
-                System.out.println(pair.getKey() + "-" + new String(pair.getValue().getPayload()));
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                System.out.println(e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("OnComplete");
-            }
-        });
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("enter quit to quit: ");
-            String line = scanner.nextLine();
-            if ("quit".equals(line)) {
-                if (disposable.get() != null) {
-                    disposable.get().dispose();
-                }
-                mqtt.unsubscribe(publish_topic).blockingAwait();
-                mqtt.disconnect().blockingAwait();
-                break;
-            } else {
-                //发送
-                MqttMessage mm = new MqttMessage();
-                mm.setRetained(true);
-                mm.setQos(2);
-                mm.setPayload("".equals(line) ? new byte[0] : line.getBytes());
-                mqtt.publishWithResponse(publish_topic, mm)
-                        .subscribe(new Observer<IMqttDeliveryToken>() {
-                            @Override
-                            public void onSubscribe(Disposable d) {
-
-                            }
-
-                            @Override
-                            public void onNext(IMqttDeliveryToken iMqttDeliveryToken) {
-                                //等待执行结束
-                                try {
-                                    iMqttDeliveryToken.waitForCompletion();
-                                } catch (MqttException e) {
-                                    System.err.println("等待发送完成错误:" + e.getMessage());
-                                }
-                                if (iMqttDeliveryToken.isComplete()) {
-                                    Exception exception = iMqttDeliveryToken.getException();
-                                    if (exception == null) {
-                                        System.out.println("发送成功");
-                                    } else {
-                                        System.err.println("发送错误:" + exception.getMessage());
-                                    }
-                                } else {
-                                    System.err.println("发送未完成");
-                                }
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-
-                            }
-
-                            @Override
-                            public void onComplete() {
-
-                            }
-                        });
-            }
-        }
-        System.exit(1);
+  public static void main(String[] args) throws IOException {
+    String name = "application.properties";
+    InputStream inner = Client.class.getClassLoader().getResourceAsStream(name);
+    if (inner == null) {
+      System.err.println("can not find outer application.properties.");
+    } else {
+      System.out.println("load inner properties.");
+      System.getProperties().load(inner);
     }
+    String jarDir = System.getProperty("user.dir");
+    System.out.println("jarDir=" + jarDir);
+    File file = new File(jarDir + File.separator + name);
+    System.out.println("outer properties=" + file.getPath());
+    if (file.exists()) {
+      InputStream outer = new FileInputStream(file);
+      System.out.println("load outer properties.");
+      System.getProperties().load(outer);
+      outer.close();
+      String active = System.getProperty("active", "inner");
+      File[] activeFiles = new File(jarDir).listFiles(new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+          return name.endsWith(".properties") && name.contains("-") && name.substring(name.indexOf("-") + 1, name.indexOf(".")).equals(active);
+        }
+      });
+      if (activeFiles != null && activeFiles.length > 0) {
+        InputStream activeFile = new FileInputStream(activeFiles[0]);
+        System.out.println("load active properties.");
+        System.getProperties().load(activeFile);
+        activeFile.close();
+      } else {
+        System.err.println("can not find active application.properties.");
+      }
+    } else {
+      System.err.println("can not find outer application.properties.");
+    }
+    String host = System.getProperty("host", "tcp://10.1.1.122:1883");
+    String id = System.getProperty("id", "mqtt");
+    String username = System.getProperty("username", "admin");
+    String password = System.getProperty("password", "public");
+    String publish_topic = System.getProperty("publish_topic", "mqtt_publish");
+    String subscribe_topic = System.getProperty("subscribe_topic", "mqtt_subscribe");
+    System.out.println(">>host=" + host);
+    System.out.println(">>username=" + username);
+    System.out.println(">>password=" + password);
+    System.out.println(">>id=" + id);
+    System.out.println(">>publish_topic=" + publish_topic);
+    System.out.println(">>subscribe_topic=" + subscribe_topic);
+    MQTT mqtt = new MQTT();
+    IOptions iOptions = new IOptions.Builder().setHost(host).setId(id).build();
+    iOptions.getOptions().setUserName(username);
+    iOptions.getOptions().setPassword(password.toCharArray());
+    iOptions.getOptions().setAutomaticReconnect(true);
+    mqtt.setMqttCallback(new MqttCallback() {
+      @Override
+      public void connectionLost(Throwable cause) {
+        System.err.println(cause.getMessage());
+      }
+
+      @Override
+      public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println("messageArrived: " + topic + new String(message.getPayload()));
+      }
+
+      @Override
+      public void deliveryComplete(IMqttDeliveryToken token) {
+        System.out.println("deliveryComplete: " + (token.getException() == null ? "成功" : token.getException().getMessage()));
+      }
+    });
+    //连接
+    mqtt.connect(iOptions).blockingAwait();
+    AtomicReference<Disposable> disposable = new AtomicReference<>();
+    //订阅
+    mqtt.subscribeWithTopic(subscribe_topic, 2).subscribe(new Observer<Pair<String, MqttMessage>>() {
+      @Override
+      public void onSubscribe(Disposable d) {
+        disposable.set(d);
+      }
+
+      @Override
+      public void onNext(Pair<String, MqttMessage> pair) {
+        System.out.println(pair.getKey() + "-" + Arrays.toString(pair.getValue().getPayload()));
+        System.out.println(pair.getKey() + "-" + new String(pair.getValue().getPayload()));
+      }
+
+      @Override
+      public void onError(Throwable e) {
+        System.out.println(e.getMessage());
+      }
+
+      @Override
+      public void onComplete() {
+        System.out.println("OnComplete");
+      }
+    });
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.println("enter quit to quit: ");
+      String line = scanner.nextLine();
+      if ("quit".equals(line)) {
+        if (disposable.get() != null) {
+          disposable.get().dispose();
+        }
+        mqtt.unsubscribe(publish_topic).blockingAwait();
+        mqtt.disconnect().blockingAwait();
+        break;
+      } else {
+        //发送
+        MqttMessage mm = new MqttMessage();
+        mm.setRetained(true);
+        mm.setQos(2);
+        mm.setPayload("".equals(line) ? new byte[0] : line.getBytes());
+        mqtt.publishWithResponse(publish_topic, mm)
+            .subscribe(new Observer<IMqttDeliveryToken>() {
+              @Override
+              public void onSubscribe(Disposable d) {
+
+              }
+
+              @Override
+              public void onNext(IMqttDeliveryToken iMqttDeliveryToken) {
+                //等待执行结束
+                try {
+                  iMqttDeliveryToken.waitForCompletion();
+                } catch (MqttException e) {
+                  System.err.println("等待发送完成错误:" + e.getMessage());
+                }
+                if (iMqttDeliveryToken.isComplete()) {
+                  Exception exception = iMqttDeliveryToken.getException();
+                  if (exception == null) {
+                    System.out.println("发送成功");
+                  } else {
+                    System.err.println("发送错误:" + exception.getMessage());
+                  }
+                } else {
+                  System.err.println("发送未完成");
+                }
+              }
+
+              @Override
+              public void onError(Throwable e) {
+
+              }
+
+              @Override
+              public void onComplete() {
+
+              }
+            });
+      }
+    }
+    System.exit(1);
+  }
 }
---
+++ src/main/java/mcivicm/mqtt/Connection.java
@@ -0,0 +1,193 @@
+package mcivicm.mqtt;
+
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import org.apache.commons.lang3.tuple.Pair;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Connection {
+    private String host;
+    private String clientId;
+    private String username;
+    private String password;
+    //0未连接，1正在连接，2已连接
+    private AtomicInteger isConnected = new AtomicInteger(0);
+    private OnConnectionListener listener;
+    private MQTT mqtt = new MQTT();
+    private Scheduler scheduler = Schedulers.from(Executors.newSingleThreadExecutor());
+    private List<Pair<String, MqttMessage>> toPublish = new LinkedList<>();
+
+    public Connection(String host, String clientId, String username, String password, OnConnectionListener listener) {
+        this.host = host;
+        this.clientId = clientId;
+        this.username = username;
+        this.password = password;
+        this.listener = listener;
+        this.mqtt.setMqttCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                isConnected.set(0);
+                connectTask().subscribe(new SimpleCompletableObserver());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+            }
+        });
+        //开始连接
+        connectTask().subscribe(new SimpleCompletableObserver());
+    }
+
+    public Completable disconnect() {
+        return this.mqtt.disconnect().subscribeOn(scheduler);
+    }
+
+    /**
+     * 发布
+     *
+     * @param topic
+     * @param mqttMessage
+     * @return
+     */
+    public Observable<IMqttDeliveryToken> publish(String topic, MqttMessage mqttMessage) {
+        if (isConnected.get() == 2) {
+            return mqtt.publishWithResponse(topic, mqttMessage)
+                    .doOnNext(new Consumer<IMqttDeliveryToken>() {
+                        @Override
+                        public void accept(IMqttDeliveryToken iMqttDeliveryToken) throws Exception {
+                            iMqttDeliveryToken.waitForCompletion();
+                        }
+                    })
+                    .subscribeOn(scheduler);
+        } else {
+            //cache.
+            toPublish.add(Pair.of(topic, mqttMessage));
+            //do nothing.
+            return Observable.empty();
+        }
+    }
+
+    /**
+     * 订阅
+     *
+     * @param topic
+     * @param qos
+     * @return
+     */
+    public Observable<Pair<String, MqttMessage>> subscribe(final String topic, final int qos) {
+        if (isConnected.get() == 2) {
+            return mqtt.subscribeWithTopic(topic, qos).subscribeOn(scheduler);
+        } else {
+            return Observable.empty();
+        }
+    }
+
+    /**
+     * 连接
+     *
+     * @return
+     */
+    private Completable connectTask() {
+        return Observable
+                .fromCallable(new Callable<IOptions>() {
+                    @Override
+                    public IOptions call() throws Exception {
+                        //配置参数
+                        IOptions iOptions = new IOptions.Builder().setHost(host).setId(clientId).build();
+                        iOptions.getOptions().setUserName(username);
+                        iOptions.getOptions().setPassword(password.toCharArray());
+                        return iOptions;
+                    }
+                })
+                .flatMapCompletable(new Function<IOptions, CompletableSource>() {
+                    @Override
+                    public CompletableSource apply(IOptions iOptions) throws Exception {
+                        //连接
+                        return mqtt
+                                .connect(iOptions)
+                                .doOnSubscribe(new Consumer<Disposable>() {
+                                    @Override
+                                    public void accept(Disposable disposable) throws Exception {
+                                        isConnected.set(1);
+                                    }
+                                })
+                                .doOnComplete(new Action() {
+                                    @Override
+                                    public void run() throws Exception {
+                                        isConnected.set(2);
+                                        //发送连接成功的消息
+                                        if (listener != null) {
+                                            //订阅该订阅的主题
+                                            listener.onConnected(mqtt);
+                                        }
+                                        publishRemnant();
+                                    }
+                                })
+                                .doOnError(new Consumer<Throwable>() {
+                                    @Override
+                                    public void accept(Throwable throwable) throws Exception {
+                                        isConnected.set(0);
+                                    }
+                                });
+                    }
+                })
+                //在后台进行
+                .subscribeOn(scheduler);
+    }
+
+    /**
+     * 发送由于未连接或者连接中而缓存起来的消息
+     */
+    private void publishRemnant() {
+        if (toPublish.size() > 0) {
+            ListIterator<Pair<String, MqttMessage>> iterator = toPublish.listIterator();
+            while (iterator.hasNext()) {
+                Pair<String, MqttMessage> next = iterator.next();
+                publish(next.getKey(), next.getValue()).subscribe(new PublishObserver());
+                iterator.remove();
+            }
+        }
+    }
+
+    public interface OnConnectionListener {
+        void onConnected(MQTT mqtt);
+    }
+
+    private class SimpleCompletableObserver implements CompletableObserver {
+
+        @Override
+        public void onComplete() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onSubscribe(Disposable d) {
+
+        }
+    }
+
+}
